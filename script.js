d3237d5a8ae19f42b9859f8346f9fcacc39b06c0
Михаил <michael.codedev@gmail.com>
2019-06-19 21:31:56 +0300

    Day 2. Rewright script on fetch.

--- script.js
+++ script.js
@@ -8,9 +8,11 @@ function apiSearch(event) {
 		server = "https://api.themoviedb.org/3/search/multi?api_key=57c852ff879035f79066cb8a16f7e4cb&language=ru&query=" +
 		searchText + "&include_adult=true";
 
-	requestApi("GET", server)
-		.then(function (result) {
-			const outPut = JSON.parse(result);
+	fetch(server)
+		.then(function (value) {
+			return value.json();
+		})
+		.then(function (outPut) {
 			let inner = "";
 
 			outPut.results.forEach(item => {
@@ -20,73 +22,15 @@ function apiSearch(event) {
 				inner += `<div class='col-12 p-3 mb-3 border bg-white rounded d-flex flex-row flex-wrap'>
 			<div class='col-6'>${nameItem}</div><div class='col-6'>Дата выхода: ${firstDate}</div>
 			<div class='col-12 mt-3'>${item.overview}</div></div>`;
-			});
 
-			movies.innerHTML = inner;
-
-			console.log(outPut);
-		}).catch(function (reason) {
+				movies.innerHTML = inner;
+				console.log(outPut);
+			})
+		})
+		.catch(function (reason) {
 			movies.innerHTML = "Упс, что то пошло не так:(";
 			console.log("error: " + reason.status);
 		});
 }
 
 searchForm.addEventListener("submit", apiSearch);
-
-function requestApi(method, url) {
-	return new Promise(function (resolve, rejected) {
-		const request = new XMLHttpRequest();
-		request.open(method, url);
-
-		request.addEventListener('load', function (params) {
-			if (request.status !== 200) {
-				rejected({
-					status: request.status
-				});
-				return;
-			}
-
-			resolve(request.response);
-		});
-
-		request.addEventListener('error', function (params) {
-			rejected({
-				status: request.status
-			});
-		});
-
-		request.send();
-	});
-
-
-	/*
-
-	request.addEventListener("readystatechange", () => {
-		if (request.readyState !== 4) {
-			movies.innerHTML = "Загрузка";
-			return;
-		}
-		if (request.status !== 200) {
-			movies.innerHTML = "Упс, что то пошло не так:(";
-			console.log("error: " + request.status);
-			return;
-		}
-
-		const outPut = JSON.parse(request.responseText);
-
-		let inner = "";
-
-		outPut.results.forEach(item => {
-			let nameItem = item.title || item.name,
-				firstDate = item.release_date || item.first_air_date;
-
-			inner += `<div class='col-12 p-3 mb-3 border bg-white rounded d-flex flex-row flex-wrap'>
-			<div class='col-6'>${nameItem}</div><div class='col-6'>Дата выхода: ${firstDate}</div>
-			<div class='col-12 mt-3'>${item.overview}</div></div>`;
-		});
-
-		movies.innerHTML = inner;
-
-		console.log(outPut);
-	});*/
-}
