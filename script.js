84d1fe630527fa06a2b25ccd348124d1dbe6eb74
Михаил <michael.codedev@gmail.com>
2019-06-20 14:03:38 +0300

    Day 2. Added empty block for absent images

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function apiSearch(event) {
 
 	const searchText = document.getElementById("search-text").value,
 		server = "https://api.themoviedb.org/3/search/multi?api_key=57c852ff879035f79066cb8a16f7e4cb&language=ru&query=" +
-		searchText + "&include_adult=true";
+		searchText;// "&include_adult=true";
 
 	fetch(server)
 		.then(function (value) {
@@ -18,14 +18,20 @@ function apiSearch(event) {
 			let inner = "";
 
 			outPut.results.forEach(item => {
-				let nameItem = item.title || item.name,
-					firstDate = item.release_date || item.first_air_date;
+				let nameItem = item.title || item.name;
+				let firstDate = item.release_date || item.first_air_date;
 
-				inner += `<div class='col-12 p-3 mb-3 border bg-white rounded d-flex flex-row flex-wrap'>
-			<div class='col-12'>${nameItem}</div>
-			<div class='col-12'>Дата выхода: ${firstDate}</div>
-			<img src="${urlPoster + item.poster_path}" alt="${nameItem}" class="col-6"></img>
-			<div class='col-12 mt-3'>${item.overview}</div></div>`;
+				let poster;
+				if (item.poster_path == null){
+					poster = `<div class="col-12"><div class="poster"></div></div>`;
+				} else{
+					poster = `<div class="col-12"><img src="${urlPoster + item.poster_path}" alt="${nameItem}"></img></div>`;
+				}
+
+				inner += `<div class="col-4"><div class='col-12 mb-3 p-3 border bg-white rounded'>
+				<div class='col-12'>${nameItem}</div>
+				<div class='col-12 mb-3'>Дата выхода: ${firstDate}</div>
+				${poster}</div></div>`;
 
 				movies.innerHTML = inner;
 			});
